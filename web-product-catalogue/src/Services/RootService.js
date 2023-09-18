dd95df63453b3b04b6883a8385135661d1ab7ade
Lee Codes <111055500+Lerato-Lee@users.noreply.github.com>
2023-09-18 07:36:08 +0200

    Delete web-product-catalogue directory

--- web-product-catalogue/src/Services/RootService.js
+++
@@ -1,9 +0,0 @@
-export const BaseApiUrl = "https://localhost:5001";
-
-export const AuthenticationPath = `/api/auth/authenticate`;
-export const ProductCatalogSavePath = `/api/productcatalogs/create`;
-export const ProductCatalogUpdatePath = `/api/productcatalogs/update`;
-export const ProductCatalogGetItemPath = `/api/productcatalogs/get`;
-export const ProductCatalogListPath = `/api/productcatalogs/getall`;
-export const ProductCatalogDeletePath = `/api/productcatalogs/delete`;
-export const ProductCatalogImportPath = `/api/productcatalogs/import`;
